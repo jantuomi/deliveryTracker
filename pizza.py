35e135c9dc8dcb9ccdce34a8818e051b5bd04281
bwalterscoding <40584788+bwalterscoding@users.noreply.github.com>
2018-08-05 00:25:44 -0400

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,14 @@
+distance = float(input ("Give me your round trip distance in miles: "))
+wear = 0.1
+print(distance * wear)
+print = float(input ("How much is the order: "))
+price = input
+print = float(input ("Give me the total amount given to you: "))
+amount = input
+print (amount - price)
+print = ("After gas and wear-n-tear, your net tip is " + 2.6 / 30 * distance ) #price per gallon / mpg * distance
+
+
+
+
+
